da313dd6ee40a594638a0f96a2e3922db067d136
Felix <leimenstoll.felix@gmail.com>
2025-05-20 20:53:30 +0200

    wrapped streaming step into a loop

--- executables/02/main.cpp
+++ executables/02/main.cpp
@@ -1,10 +1,6 @@
 #include "streaming.cuh"
-
-#include <cassert>
 #include <cuda_runtime.h>
-#include <device_launch_parameters.h>
 #include <spdlog/spdlog.h>
-#include <vector>
 
 
 
@@ -14,11 +10,9 @@ int main(int argc, char* argv[])
     // [hour:min:sec.ms] [file.cpp:line] [type] [message]
     spdlog::set_pattern("[%T.%e] [%s:%#] [%^%l%$] %v");
 
-    SPDLOG_INFO("Starting up milestone02...");
-
     // grid size (number of lattice cells per dimension)
-    int grid_width =   300;
-    int grid_height =  200;
+    int grid_width =   3000;
+    int grid_height =  2000;
 
     // misc
     int num_cells = grid_width * grid_height;
@@ -52,19 +46,25 @@ int main(int argc, char* argv[])
     // alternatively, use optimized CUDA function to initialize values to zero
     // cudaMemset(dvc_distributionFunc, 0, num_cells * num_dirs * sizeof(float));
 
-    // launch kernel for computing the density field
-    Launch_ComputeDensityField_K(
-        dvc_distributionFunc, dvc_densityField, num_cells);
+    // run the streaming step for a specified number of iterations
+    for (int step = 0; step < 1000; step++)
+    {
+        // launch kernel for computing the density field
+        Launch_ComputeDensityField_K(
+            dvc_distributionFunc, dvc_densityField, num_cells);
 
-    // launch kernel for computing the velocity field
-    Launch_ComputeVelocityField_K(
-        dvc_distributionFunc, dvc_densityField, dvc_velocityField_x,
-        dvc_velocityField_y, num_cells);
+        // launch kernel for computing the velocity field
+        Launch_ComputeVelocityField_K(
+            dvc_distributionFunc, dvc_densityField, dvc_velocityField_x,
+            dvc_velocityField_y, num_cells);
 
-    // launch kernel for the streaming step
-    Launch_StreamingStep_K(
-        dvc_distributionFunc, dvc_distributionFunc_next, grid_width,
-        grid_height, num_cells);
+        // launch kernel for the streaming step
+        Launch_StreamingStep_K(
+            dvc_distributionFunc, dvc_distributionFunc_next, grid_width,
+            grid_height, num_cells);
+
+        std::swap(dvc_distributionFunc, dvc_distributionFunc_next);
+    }
 
     // free device memory
     cudaFree(dvc_distributionFunc);
