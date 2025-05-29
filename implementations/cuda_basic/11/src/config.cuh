b807278829b8b3b136c91bcb4acdc6cfe3c2b9f0
Felix <leimenstoll.felix@gmail.com>
2025-05-29 14:51:44 +0200

    added implementation 11 with sequential, non-coalesced, cpu-like memory reads per thread with high cache hit rate for comparison

---
+++ implementations/cuda_basic/11/src/config.cuh
@@ -0,0 +1,7 @@
+#pragma once
+#include <cstdint>
+
+
+
+constexpr uint32_t N_DIR =          9;
+constexpr uint32_t N_BLOCKSIZE =    256;
--- implementations/cuda_basic/11/src/main.cpp
+++ implementations/cuda_basic/11/src/main.cpp
@@ -1,5 +1,5 @@
 // CUDA implementation of Lattice-Boltzmann using optimization strategies:
-// - coalesced memory reads
+// - sequential, non-coalesced memory reads per thread for high cache hit rates
 // - shared memory tiles for df values
 // - fully fused density/velocity/collision/streaming kernel (push)
 // - no global write-back of density and velocity values
@@ -41,48 +41,19 @@ int main(int argc, char* argv[])
     float n = 1.0f;
     float k = (2.0f * PI * n) / static_cast<float>(N_Y);
 
-    // host-side arrays of 9 pointers to device-side df arrays
-    float* df[9];
-    float* df_next[9];
-
-    // for each dir i, allocate 1D array of size N_CELLS on the device
-    for (uint32_t i = 0; i < 9; i++)
-    {
-        cudaMalloc(&df[i], N_CELLS * sizeof(float));
-        cudaMalloc(&df_next[i], N_CELLS * sizeof(float));
-    }
-
-    // device-side arrays of 9 pointers to device-side df arrays
-    // (used as a device-side handle for the SoA data)
-    float** dvc_df;
-    float** dvc_df_next;
-    cudaMalloc(&dvc_df, 9 * sizeof(float*));
-    cudaMalloc(&dvc_df_next, 9 * sizeof(float*));
-
-    // copy the contents of the host-side handles to the device-side handles
-    // (because apparently CUDA does not support directly passing an array of pointers)
-    cudaMemcpy(dvc_df, df, 9 * sizeof(float*), cudaMemcpyHostToDevice);
-    cudaMemcpy(dvc_df_next, df_next, 9 * sizeof(float*), cudaMemcpyHostToDevice);
-
-    // pointers to the device-side density and velocity arrays
+    // pointers to the device-side df, density, and velocity arrays
+    float* dvc_df;
+    float* dvc_df_next;
     float* dvc_rho;
     float* dvc_u_x;
     float* dvc_u_y;
 
-    // for each array, allocate memory of size N_CELLS on the device
+    // allocate memory on the device
+    cudaMalloc(&dvc_df, 9 * N_CELLS * sizeof(float));
+    cudaMalloc(&dvc_df_next, 9 * N_CELLS * sizeof(float));
     cudaMalloc(&dvc_rho, N_CELLS * sizeof(float));
     cudaMalloc(&dvc_u_x, N_CELLS * sizeof(float));
     cudaMalloc(&dvc_u_y, N_CELLS * sizeof(float));
-
-    // collect buffers and other data for export context
-    SimulationExportContext context;
-    context.dvc_df = dvc_df;
-    context.dvc_df_next = dvc_df_next;
-    context.dvc_rho = dvc_rho;
-    context.dvc_u_x = dvc_u_x;
-    context.dvc_u_y = dvc_u_y;
-    context.N_X = N_X;
-    context.N_Y = N_Y;
 
     Launch_ApplyShearWaveCondition_K(dvc_df, dvc_rho, dvc_u_x, dvc_u_y, rho_0,
         u_max, k, N_X, N_Y, N_CELLS);
@@ -101,24 +72,8 @@ int main(int argc, char* argv[])
         {
             SPDLOG_INFO("--- step {} done ---", step);
         }
-
-        // export data (CAREFUL: huge file sizes)
-        if (false && (step == 1 || step % 1000 == 0))
-        {
-            ExportSimulationData(context,
-                VelocityMagnitude,
-                "05",
-                "A",
-                step);
-        }
     }
 
-    // cleanup
-    for (uint32_t i = 0; i < 9; i++)
-    {
-        cudaFree(df[i]);
-        cudaFree(df_next[i]);
-    }
     cudaFree(dvc_df);
     cudaFree(dvc_df_next);
     cudaFree(dvc_rho);
