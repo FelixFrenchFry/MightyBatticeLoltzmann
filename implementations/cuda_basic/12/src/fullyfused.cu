85ba22fa588a549c9a46608b05acf393b70d5cb9
Felix <leimenstoll.felix@gmail.com>
2025-05-30 09:49:13 +0200

    added implementation 12 with flattened 1D arrays for distribution function values instead of 2D

---
+++ implementations/cuda_basic/12/src/fullyfused.cu
@@ -0,0 +1,137 @@
+#include "config.cuh"
+#include <cuda_runtime.h>
+#include <cstddef>
+#include <spdlog/spdlog.h>
+
+
+
+// load velocity direction and weight vectors into constant memory
+// (fast, global, read-only lookup table identical for all threads)
+// TODO: figure out how to safely use same constant memory across all .cu files
+__constant__ int dvc_c_x[9];
+__constant__ int dvc_c_y[9];
+__constant__ float dvc_w[9];
+bool constantsInitialized = false;
+
+void InitializeConstants()
+{
+    if (constantsInitialized) { return; }
+
+    //  0: ( 0,  0) = rest
+    //  1: ( 1,  0) = east
+    //  2: ( 0,  1) = north
+    //  3: (-1,  0) = west
+    //  4: ( 0, -1) = south
+    //  5: ( 1,  1) = north-east
+    //  6: (-1,  1) = north-west
+    //  7: (-1, -1) = south-west
+    //  8: ( 1, -1) = south-east
+
+    // initialize velocity direction and weight vectors on the host
+    int c_x[9] = { 0,  1,  0, -1,  0,  1, -1, -1,  1 };
+    int c_y[9] = { 0,  0,  1,  0, -1,  1,  1, -1, -1 };
+    float w[9] = { 4.0f/9.0f, 1.0f/9.0f, 1.0f/9.0f, 1.0f/9.0f, 1.0f/9.0f,
+                   1.0f/36.0f, 1.0f/36.0f, 1.0f/36.0f, 1.0f/36.0f };
+
+    // copy them into constant memory on the device
+    cudaMemcpyToSymbol(dvc_c_x, c_x, 9 * sizeof(int));
+    cudaMemcpyToSymbol(dvc_c_y, c_y, 9 * sizeof(int));
+    cudaMemcpyToSymbol(dvc_w, w, 9 * sizeof(float));
+
+    cudaDeviceSynchronize();
+    constantsInitialized = true;
+}
+
+template <uint32_t N_DIR, uint32_t N_BLOCKSIZE>
+__global__ void ComputeFullyFusedOperations_K(
+    const float* __restrict__ dvc_df_flat,
+    float* __restrict__ dvc_df_next_flat,
+    float* __restrict__ dvc_rho,
+    float* __restrict__ dvc_u_x,
+    float* __restrict__ dvc_u_y,
+    const float omega,
+    const uint32_t N_X, const uint32_t N_Y,
+    const uint32_t N_CELLS)
+{
+    uint32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx >= N_CELLS) { return; }
+
+    // used for summing stuff up and computing collision
+    float rho = 0.0f;
+    float u_x = 0.0f;
+    float u_y = 0.0f;
+
+    // density := sum over df values in each dir i
+    // velocity := sum over df values, weighted by each dir i
+    #pragma unroll
+    for (uint32_t i = 0; i < N_DIR; i++)
+    {
+        float df_i = dvc_df_flat[i * N_CELLS + idx];
+        rho += df_i;
+        u_x += df_i * dvc_c_x[i];
+        u_y += df_i * dvc_c_y[i];
+    }
+
+    // exit thread to avoid division by zero or erroneous values
+    if (rho <= 0.0f) { return; }
+
+    // finalize velocities
+    u_x /= rho;
+    u_y /= rho;
+
+    // pre-compute squared velocity and cell coordinates for this thread
+    float u_sq = u_x * u_x + u_y * u_y;
+    uint32_t src_x = idx % N_X;
+    uint32_t src_y = idx / N_X;
+
+    #pragma unroll
+    for (uint32_t i = 0; i < N_DIR; i++)
+    {
+        // compute dot product of c_i * u and equilibrium df value for dir i
+        float cu = static_cast<float>(dvc_c_x[i]) * u_x
+                 + static_cast<float>(dvc_c_y[i]) * u_y;
+        float f_eq_i = dvc_w[i] * rho
+                     * (1.0f + 3.0f * cu + 4.5f * cu * cu - 1.5f * u_sq);
+
+        // relax df towards equilibrium
+        // TODO: bug in this optimized computation?
+        float f_new_i = dvc_df_flat[i * N_CELLS + idx] * (1 - omega) + omega * f_eq_i;
+
+        // determine index of the destination cell within the SoA
+        // (with respect to periodic boundary conditions)
+        uint32_t dst_idx = ((src_y + dvc_c_y[i] + N_Y) % N_Y) * N_X
+                         + ((src_x + dvc_c_x[i] + N_X) % N_X);
+
+        // stream df value df_i to the neighbor in dir i (on the double buffer)
+        dvc_df_next_flat[i * N_CELLS + dst_idx] = f_new_i;
+    }
+}
+
+void Launch_FullyFusedOperationsComputation(
+    const float* dvc_df_flat,
+    float* dvc_df_next_flat,
+    float* dvc_rho,
+    float* dvc_u_x,
+    float* dvc_u_y,
+    const float omega,
+    const uint32_t N_X, const uint32_t N_Y,
+    const uint32_t N_CELLS)
+{
+    InitializeConstants();
+
+    const uint32_t N_GRIDSIZE = (N_CELLS + N_BLOCKSIZE - 1) / N_BLOCKSIZE;
+
+    ComputeFullyFusedOperations_K<N_DIR, N_BLOCKSIZE><<<N_GRIDSIZE, N_BLOCKSIZE>>>(
+        dvc_df_flat, dvc_df_next_flat, dvc_rho, dvc_u_x, dvc_u_y, omega, N_X, N_Y,
+        N_CELLS);
+
+    // wait for GPU to finish operations
+    cudaDeviceSynchronize();
+
+    // debugging helper
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess)
+    {
+        SPDLOG_ERROR("CUDA kernel failed: {}", cudaGetErrorString(err));
+    }
+}
---
+++ implementations/cuda_basic/12/src/main.cpp
@@ -0,0 +1,107 @@
+// CUDA implementation of Lattice-Boltzmann using optimization strategies:
+// - coalesced memory reads TODO: AND coalesced memory writes
+// - TODO: two different kernels, adjusted for the respective df data structure
+// - shared memory tiles for df values
+// - fully fused density/velocity/collision/streaming kernel (push)
+// - no global write-back of density and velocity values
+
+#include "../../tools/export.h"
+#include "../../tools/utilities.h"
+#include "fullyfused.cuh"
+#include "initialization.cuh"
+#include <cuda_runtime.h>
+#include <iostream>
+#include <spdlog/spdlog.h>
+
+
+
+int main(int argc, char* argv[])
+{
+    // configure spdlog to display error messages like this:
+    // [hour:min:sec.ms] [file.cpp:line] [type] [message]
+    spdlog::set_pattern("[%T.%e] [%s:%#] [%^%l%$] %v");
+
+    constexpr float PI = 3.14159265f;
+
+    // grid width, height, number of simulation steps, number of grid cells
+    // (84 bytes per cell -> 15,000 * 10,000 cells use ~12GB of VRAM)
+    uint32_t N_X =      15000;
+    uint32_t N_Y =      10000;
+    uint32_t N_STEPS =  1;
+    uint32_t N_CELLS =  N_X * N_Y;
+
+    // relaxation factor, rest density, max velocity, number of sine periods,
+    // wavenumber (frequency)
+    float omega = 1.2f;
+    float rho_0 = 1.0f;
+    float u_max = 0.1f;
+    float n = 1.0f;
+    float k = (2.0f * PI * n) / static_cast<float>(N_Y);
+
+    // pointers to the device-side flattened 1D df value arrays
+    float* dvc_df_flat;
+    float* dvc_df_next_flat;
+    cudaMalloc(&dvc_df_flat, 9 * N_CELLS * sizeof(float));
+    cudaMalloc(&dvc_df_next_flat, 9 * N_CELLS * sizeof(float));
+
+    // pointers to the device-side density and velocity arrays
+    float* dvc_rho;
+    float* dvc_u_x;
+    float* dvc_u_y;
+
+    // for each array, allocate memory of size N_CELLS on the device
+    cudaMalloc(&dvc_rho, N_CELLS * sizeof(float));
+    cudaMalloc(&dvc_u_x, N_CELLS * sizeof(float));
+    cudaMalloc(&dvc_u_y, N_CELLS * sizeof(float));
+
+    // collect buffers and other data for export context
+    SimulationExportContext context;
+    //context.dvc_df = dvc_df;
+    //context.dvc_df_next = dvc_df_next;
+    context.dvc_rho = dvc_rho;
+    context.dvc_u_x = dvc_u_x;
+    context.dvc_u_y = dvc_u_y;
+    context.N_X = N_X;
+    context.N_Y = N_Y;
+
+    DisplayDeviceModel();
+    DisplayDeviceMemoryUsage();
+
+    Launch_ApplyShearWaveCondition_K(dvc_df_flat, dvc_rho, dvc_u_x, dvc_u_y, rho_0,
+        u_max, k, N_X, N_Y, N_CELLS);
+
+    for (uint32_t step = 1; step <= N_STEPS; step++)
+    {
+        // update densities and velocities, update df_i values based on
+        // densities and velocities and move them to neighboring cells
+        Launch_FullyFusedOperationsComputation(
+            dvc_df_flat, dvc_df_next_flat, dvc_rho, dvc_u_x, dvc_u_y, omega, N_X, N_Y,
+            N_CELLS);
+
+        std::swap(dvc_df_flat, dvc_df_next_flat);
+
+        if (step == 1 || step % 100 == 0)
+        {
+            SPDLOG_INFO("--- step {} done ---", step);
+        }
+
+        // export data (CAREFUL: huge file sizes)
+        if (false && (step == 1 || step % 1000 == 0))
+        {
+            ExportSimulationData(context,
+                VelocityMagnitude,
+                "05",
+                "A",
+                step);
+        }
+    }
+
+    // cleanup
+    cudaFree(dvc_df_flat);
+    cudaFree(dvc_df_next_flat);
+    cudaFree(dvc_rho);
+    cudaFree(dvc_u_x);
+    cudaFree(dvc_u_y);
+
+    return 0;
+}
