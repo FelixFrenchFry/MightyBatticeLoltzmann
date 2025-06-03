4d76db9a8680f46ad7fb006af0424f67fa27de86
Felix <leimenstoll.felix@gmail.com>
2025-06-03 17:36:07 +0200

    adjusted python script naming scheme

--- implementations/cuda_basic/tools/visualizer_03.py
+++ implementations/cuda_basic/tools/visualizer_03.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 
 
 
-# ----- VISUALIZATION OF THE X-VELOCITY DECAY (WAVE DECAY) -----
+# ----- VISUALIZATION OF THE X-VELOCITY DECAY (AMPLITUDE DECAY) -----
 # simulation config
 #steps = [1, 25000, 50000, 75000, 100000, 125000, 150000, 175000, 200000,
 #         225000, 250000, 275000, 300000, 325000, 350000, 375000, 400000]
 #steps = [1, 1000, 2000, 3000, 4000, 5000, 6000]
-steps = [1, 100000]#, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000]
+steps = [1, 100000, 200000, 300000, 400000, 500000]#, 600000, 700000, 800000, 900000, 1000000]
 N_X =   15000
 N_Y =   10000
 
@@ -31,29 +31,28 @@ def get_file_path(step: int) -> str:
 
 # load u_x along the y-axis at fixed x-position across the time steps
 center_x = N_X // 2
-profiles = []
+amplitudes = []
 for step in steps:
     data = np.fromfile(get_file_path(step), dtype=np.float32).reshape((N_Y, N_X))
-    u_x_y = data[:, center_x]  # vertical slice at middle x value
-    profiles.append(u_x_y)
+    u_x_y = data[:, center_x]
+    amplitude = np.max(np.abs(u_x_y))
+    amplitudes.append(amplitude)
 
-# plot decay over time
-plt.figure(figsize=(8, 4))
-for i, profile in enumerate(profiles):
-    label = f"Step {steps[i]}"
-    plt.plot(profile, label=label)
+amplitudes = np.array(amplitudes)
+normalized_amp = amplitudes / amplitudes[0]
 
 # plot settings
 outputDir = f"{outputDirName}/{versionDirName}/{subDirName}"
 os.makedirs(outputDir, exist_ok=True)
-plt.title("Wave decay of X-velocity (u_x) along Y-axis")
-plt.xlabel("Y")
-plt.ylabel("u_x")
-plt.legend(loc='upper right', fontsize=8)
+plt.figure(figsize=(8, 4))
+plt.plot(steps, normalized_amp)
+plt.title("Amplitude decay of X-velocity (u_x)")
+plt.xlabel("timestep")
+plt.ylabel("amplitude")
 plt.grid(True)
 
 # save plot
-outputPath = f"{outputDir}/wave_decay_x{format_step_suffix(steps[-1])}.png"
+outputPath = f"{outputDir}/amplitude_decay_x{format_step_suffix(steps[-1])}.png"
 plt.savefig(outputPath, dpi=300)
 plt.close()
 print(f"Saved plot: {outputPath}")
