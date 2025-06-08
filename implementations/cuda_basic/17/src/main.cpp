aedc9fa74e3499472888ea84930c1ee6797e4919
Felix <leimenstoll.felix@gmail.com>
2025-06-09 01:21:31 +0200

    added implementation 17, push-style streaming with shared memory tiling populated in density/velocity loop

---
+++ implementations/cuda_basic/17/src/main.cpp
@@ -0,0 +1,157 @@
+// CUDA implementation of Lattice-Boltzmann with notable properties:
+// - coalesced memory accesses of df values
+// - shared memory tiling for df values
+// - fully fused density/velocity/collision/streaming kernel (push)
+// - no global write-back of density and velocity values
+// - inlined sub-kernels for modularity (no performance impact)
+// - lid-driven cavity with bounce-back boundary conditions
+// - fp32/fp64 precision switch at compile-time for df, density, velocity values
+// - int and fp versions of the directions vectors to avoid casting
+// - TODO: reduce register pressure and optimize for register spills
+
+#include "../../tools/config.cuh"
+#include "../../tools/data_export.h"
+#include "../../tools/utilities.h"
+#include "initialization.cuh"
+#include "simulation.cuh"
+#include <cuda_runtime.h>
+#include <spdlog/spdlog.h>
+
+
+
+int main(int argc, char* argv[])
+{
+    // configure spdlog to display error messages like this:
+    // [year-month-day hour:min:sec] [type] [message]
+    spdlog::set_pattern("[%Y-%m-%d %H:%M:%S] [%^%l%$] %v");
+
+    // grid width, height, number of simulation steps, number of grid cells
+    // (84 bytes per cell -> 15,000 * 10,000 cells use ~12GB of VRAM)
+    uint32_t N_X =      15000;
+    uint32_t N_Y =      10000;
+    uint32_t N_STEPS =  5000;
+    uint32_t N_CELLS =  N_X * N_Y;
+
+    // relaxation factor, rest density, max velocity, number of sine periods,
+    // wavenumber (frequency), lid velocity
+    FP omega = 1.2;
+    FP rho_0 = 1.0;
+    FP u_max = 0.1;
+    FP n = 3.0;
+    FP k = (FP_CONST(2.0) * FP_PI * n) / static_cast<FP>(N_Y);
+    FP u_lid = 0.1;
+
+    // data export settings
+    bool write_rho =    false;
+    bool write_u_x =    true;
+    bool write_u_y =    true;
+
+    // simulation settings
+    bool shear_wave_decay =     false;
+    bool lid_driven_cavity =    true;
+
+    // host-side arrays of 9 pointers to device-side df arrays
+    FP* df[9];
+    FP* df_next[9];
+
+    // for each dir i, allocate 1D array of size N_CELLS on the device
+    for (uint32_t i = 0; i < 9; i++)
+    {
+        cudaMalloc(&df[i], N_CELLS * sizeof(FP));
+        cudaMalloc(&df_next[i], N_CELLS * sizeof(FP));
+    }
+
+    // device-side arrays of 9 pointers to device-side df arrays
+    // (used as a device-side handle for the SoA data)
+    FP** dvc_df;
+    FP** dvc_df_next;
+    cudaMalloc(&dvc_df, 9 * sizeof(FP*));
+    cudaMalloc(&dvc_df_next, 9 * sizeof(FP*));
+
+    // copy the contents of the host-side handles to the device-side handles
+    // (because apparently CUDA does not support directly passing an array of pointers)
+    cudaMemcpy(dvc_df, df, 9 * sizeof(FP*), cudaMemcpyHostToDevice);
+    cudaMemcpy(dvc_df_next, df_next, 9 * sizeof(FP*), cudaMemcpyHostToDevice);
+
+    // pointers to the device-side density and velocity arrays
+    FP* dvc_rho;
+    FP* dvc_u_x;
+    FP* dvc_u_y;
+
+    // for each array, allocate memory of size N_CELLS on the device
+    cudaMalloc(&dvc_rho, N_CELLS * sizeof(FP));
+    cudaMalloc(&dvc_u_x, N_CELLS * sizeof(FP));
+    cudaMalloc(&dvc_u_y, N_CELLS * sizeof(FP));
+
+    // collect buffers and other data for export context
+    SimulationExportContext context;
+    context.dvc_df = dvc_df;
+    context.dvc_df_next = dvc_df_next;
+    context.dvc_rho = dvc_rho;
+    context.dvc_u_x = dvc_u_x;
+    context.dvc_u_y = dvc_u_y;
+    context.N_X = N_X;
+    context.N_Y = N_Y;
+
+    DisplayDeviceModel();
+    DisplayDeviceMemoryUsage();
+
+    if (shear_wave_decay)
+    {
+        Launch_ApplyShearWaveCondition_K(dvc_df, dvc_rho, dvc_u_x, dvc_u_y,
+            rho_0,u_max, k, N_X, N_Y, N_CELLS);
+    }
+    else if (lid_driven_cavity)
+    {
+        Launch_ApplyLidDrivenCavityCondition_K(dvc_df, dvc_rho, dvc_u_x,
+            dvc_u_y, rho_0, N_CELLS);
+    }
+
+    auto start_time = std::chrono::steady_clock::now();
+
+    for (uint32_t step = 1; step <= N_STEPS; step++)
+    {
+        // compute densities and velocities, update df_i values based on
+        // densities and velocities and move them to neighboring cells
+        Launch_FullyFusedOperationsComputation(
+            dvc_df, dvc_df_next, dvc_rho, dvc_u_x, dvc_u_y, omega, u_lid,
+            N_X, N_Y, N_CELLS, write_rho, write_u_x, write_u_y);
+
+        std::swap(dvc_df, dvc_df_next);
+
+        DisplayProgressBar(step, N_STEPS);
+
+        // export data (CAREFUL: huge file sizes)
+        if (false && (step == 1 || step % 5000 == 0))
+        {
+            ExportSimulationData(context,
+                Velocity_X,
+                "13",
+                "M",
+                step);
+
+            ExportSimulationData(context,
+                Velocity_Y,
+                "13",
+                "M",
+                step);
+        }
+    }
+
+    auto end_time = std::chrono::steady_clock::now();
+    DisplayPerformanceStats(start_time, end_time, N_X, N_Y, N_STEPS);
+
+    // cleanup
+    for (uint32_t i = 0; i < 9; i++)
+    {
+        cudaFree(df[i]);
+        cudaFree(df_next[i]);
+    }
+    cudaFree(dvc_df);
+    cudaFree(dvc_df_next);
+    cudaFree(dvc_rho);
+    cudaFree(dvc_u_x);
+    cudaFree(dvc_u_y);
+
+    return 0;
+}
---
+++ implementations/cuda_basic/17/src/simulation.cu
@@ -0,0 +1,279 @@
+#include "../../tools/config.cuh"
+#include "../../tools/utilities.h"
+#include <cuda_runtime.h>
+#include <cstddef>
+#include <spdlog/spdlog.h>
+
+
+
+// load opposite direction vectors for bounce-back, velocity direction vectors,
+// and weight vectors into constant memory
+// (fast, global, read-only lookup table identical for all threads)
+// TODO: figure out how to safely use same constant memory across all .cu files
+__constant__ int dvc_opp_dir[9];
+__constant__ int dvc_c_x[9];
+__constant__ int dvc_c_y[9];
+__constant__ FP dvc_fp_c_x[9];
+__constant__ FP dvc_fp_c_y[9];
+__constant__ FP dvc_w[9];
+bool constantsInitialized =        false;
+bool kernelAttributesDisplayed =   false;
+
+void InitializeConstants()
+{
+    if (constantsInitialized) { return; }
+
+    //  0: ( 0,  0) = rest
+    //  1: ( 1,  0) = east
+    //  2: ( 0,  1) = north
+    //  3: (-1,  0) = west
+    //  4: ( 0, -1) = south
+    //  5: ( 1,  1) = north-east
+    //  6: (-1,  1) = north-west
+    //  7: (-1, -1) = south-west
+    //  8: ( 1, -1) = south-east
+
+    // initialize opposite direction, velocity direction, and weight vectors
+    int opp_dir[9] = { 0, 3, 4, 1, 2, 7, 8, 5, 6 };
+    int c_x[9] = { 0,  1,  0, -1,  0,  1, -1, -1,  1 };
+    int c_y[9] = { 0,  0,  1,  0, -1,  1,  1, -1, -1 };
+    FP fp_c_x[9] = { 0.0,  1.0,  0.0, -1.0,  0.0,  1.0, -1.0, -1.0,  1.0 };
+    FP fp_c_y[9] = { 0.0,  0.0,  1.0,  0.0, -1.0,  1.0,  1.0, -1.0, -1.0 };
+    FP w[9] = { 4.0/9.0, 1.0/9.0, 1.0/9.0, 1.0/9.0, 1.0/9.0,
+                1.0/36.0, 1.0/36.0, 1.0/36.0, 1.0/36.0 };
+
+    // copy them into constant memory on the device
+    cudaMemcpyToSymbol(dvc_opp_dir, opp_dir, 9 * sizeof(int));
+    cudaMemcpyToSymbol(dvc_c_x, c_x, 9 * sizeof(int));
+    cudaMemcpyToSymbol(dvc_c_y, c_y, 9 * sizeof(int));
+    cudaMemcpyToSymbol(dvc_fp_c_x, fp_c_x, 9 * sizeof(FP));
+    cudaMemcpyToSymbol(dvc_fp_c_y, fp_c_y, 9 * sizeof(FP));
+    cudaMemcpyToSymbol(dvc_w, w, 9 * sizeof(FP));
+
+    cudaDeviceSynchronize();
+    constantsInitialized = true;
+}
+
+__device__ __forceinline__ void ComputeNeighborIndex_PeriodicBoundary_K(
+    uint32_t src_x, uint32_t src_y,
+    uint32_t N_X, uint32_t N_Y,
+    uint32_t i,
+    uint32_t& dst_idx,
+    uint32_t& dst_i)
+{
+    // determine index of the destination neihbor cell
+    // (with respect to periodic boundary conditions)
+    dst_idx = ((src_y + dvc_c_y[i] + N_Y) % N_Y) * N_X
+            + ((src_x + dvc_c_x[i] + N_X) % N_X);
+    dst_i = i;
+}
+
+__device__ __forceinline__ void ComputeNeighborIndex_BounceBackBoundary_Conditional_K(
+    uint32_t src_x, uint32_t src_y,
+    uint32_t N_X, uint32_t N_Y,
+    uint32_t i,
+    uint32_t& dst_idx,
+    uint32_t& dst_i)
+{
+    // check if directed into a wall
+    if ((dvc_c_x[i] == -1 && src_x == 0) ||        // into left wall
+        (dvc_c_x[i] ==  1 && src_x == N_X - 1) ||  // into right wall
+        (dvc_c_y[i] == -1 && src_y == 0) ||        // into bottom wall
+        (dvc_c_y[i] ==  1 && src_y == N_Y - 1))    // into top wall
+    {
+        // same cell but opposite direction because of bounce-back
+        dst_idx = src_y * N_X + src_x;
+        dst_i = dvc_opp_dir[i];
+    }
+    else
+    {
+        // normal neighbor in direction i
+        dst_idx = (src_y + dvc_c_y[i]) * N_X + (src_x + dvc_c_x[i]);
+        dst_i = i;
+    }
+}
+
+__device__ __forceinline__ void ComputeNeighborIndex_BounceBackBoundary_BranchLess_K(
+    uint32_t src_x, uint32_t src_y,
+    uint32_t N_X, uint32_t N_Y,
+    uint32_t i,
+    uint32_t& dst_idx,
+    uint32_t& dst_i)
+{
+    // TODO: this increases register pressure by too much
+
+    // branch-less bit-wise computation of: 1 if bounce-back, else 0
+    int bounce =
+        ((dvc_c_x[i] == -1) & (src_x == 0)) |
+        ((dvc_c_x[i] ==  1) & (src_x == N_X - 1)) |
+        ((dvc_c_y[i] == -1) & (src_y == 0)) |
+        ((dvc_c_y[i] ==  1) & (src_y == N_Y - 1));
+
+    // branch-less computation of destination index
+    // TODO: reduce register usage
+    /*
+    uint32_t idx_normal = (src_y + dvc_c_y[i]) * N_X + (src_x + dvc_c_x[i]);
+    uint32_t idx_bounce = src_y * N_X + src_x;
+    dst_idx = bounce * idx_bounce + (1 - bounce) * idx_normal;
+    */
+    dst_idx = bounce * (src_y * N_X + src_x)
+            + (1 - bounce) * ((src_y + dvc_c_y[i]) * N_X + (src_x + dvc_c_x[i]));
+
+    // branch-less computation of destination direction
+    dst_i = bounce * dvc_opp_dir[i] + (1 - bounce) * i;
+}
+
+__device__ __forceinline__ void InjectLidVelocity_Conditional_K(
+    uint32_t src_y,
+    uint32_t N_Y,
+    FP rho,
+    FP omega,
+    FP u_lid,
+    uint32_t i,
+    FP& f_new_i)
+{
+    // check if directed into top wall
+    if (dvc_c_y[i] == 1 && src_y == N_Y - 1)
+    {
+        f_new_i -= FP_CONST(6.0) * omega * rho * dvc_fp_c_x[i] * u_lid;
+    }
+}
+
+__device__ __forceinline__ void InjectLidVelocity_BranchLess_K(
+    uint32_t src_y,
+    uint32_t N_Y,
+    FP rho,
+    FP omega,
+    FP u_lid,
+    uint32_t i,
+    FP& f_new_i)
+{
+    // branch-less lid velocity injection via boolean mask
+    int top_bounce = ((dvc_c_y[i] == 1) & (src_y == N_Y - 1));
+
+    f_new_i -= top_bounce * FP_CONST(6.0) * dvc_w[i] * rho
+             * dvc_fp_c_x[i] * u_lid;
+}
+
+template <uint32_t N_DIR, uint32_t N_BLOCKSIZE>
+__global__ void ComputeFullyFusedOperations_K(
+    const FP* const* __restrict__ dvc_df,
+    FP* const* __restrict__ dvc_df_next,
+    FP* __restrict__ dvc_rho,
+    FP* __restrict__ dvc_u_x,
+    FP* __restrict__ dvc_u_y,
+    const FP omega,
+    const FP u_lid,
+    const uint32_t N_X, const uint32_t N_Y,
+    const uint32_t N_CELLS,
+    const bool write_rho,
+    const bool write_u_x,
+    const bool write_u_y)
+{
+    uint32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    uint32_t tid = threadIdx.x;
+    if (idx >= N_CELLS) { return; }
+
+    // load df values into block-wise tiles of shared shared memory
+    // TODO: add 1-layer halo cells?
+    __shared__ FP tile_df[N_DIR][N_BLOCKSIZE];
+
+    // used for summing stuff up and computing collision
+    FP rho = FP_CONST(0.0);
+    FP u_x = FP_CONST(0.0);
+    FP u_y = FP_CONST(0.0);
+
+    // populate shared memory tiles and compute sums in one loop
+    // density := sum over df values in each dir i
+    // velocity := sum over df values, weighted by each dir i
+    #pragma unroll
+    for (uint32_t i = 0; i < N_DIR; i++)
+    {
+        tile_df[i][tid] = dvc_df[i][idx];
+        rho += tile_df[i][tid];
+        u_x += tile_df[i][tid] * dvc_fp_c_x[i];
+        u_y += tile_df[i][tid] * dvc_fp_c_y[i];
+    }
+
+    // exit thread to avoid division by zero or erroneous values
+    if (rho <= FP_CONST(0.0)) { return; }
+
+    // finalize velocities
+    u_x /= rho;
+    u_y /= rho;
+
+    // write back final field values only if requested
+    if (write_rho) { dvc_rho[idx] = rho; }
+    if (write_u_x) { dvc_u_x[idx] = u_x; }
+    if (write_u_y) { dvc_u_y[idx] = u_y; }
+
+    // pre-compute squared velocity and cell coordinates for this thread
+    FP u_sq = u_x * u_x + u_y * u_y;
+    uint32_t src_x = idx % N_X;
+    uint32_t src_y = idx / N_X;
+
+    #pragma unroll
+    for (uint32_t i = 0; i < N_DIR; i++)
+    {
+        // compute dot product of c_i * u and equilibrium df value for dir i
+        FP cu = dvc_fp_c_x[i] * u_x + dvc_fp_c_y[i] * u_y;
+        FP f_eq_i = dvc_w[i] * rho
+                  * (FP_CONST(1.0) + FP_CONST(3.0) * cu
+                  + FP_CONST(4.5) * cu * cu - FP_CONST(1.5) * u_sq);
+
+        // relax df towards equilibrium
+        FP f_new_i = tile_df[i][tid] - omega * (tile_df[i][tid] - f_eq_i);
+
+        // inlined sub-kernel for the neighbor index
+        uint32_t dst_idx, dst_i;
+        ComputeNeighborIndex_BounceBackBoundary_Conditional_K(
+            src_x, src_y, N_X, N_Y, i, dst_idx, dst_i);
+
+        // inject lid velocity if directed into top wall
+        InjectLidVelocity_BranchLess_K(src_y, N_Y, rho, omega, u_lid, i, f_new_i);
+
+        // stream df value df_i to the neighbor in dir i
+        // (direction i gets reversed in case of bounce-back)
+        dvc_df_next[dst_i][dst_idx] = f_new_i;
+    }
+}
+
+void Launch_FullyFusedOperationsComputation(
+    const FP* const* dvc_df,
+    FP* const* dvc_df_next,
+    FP* dvc_rho,
+    FP* dvc_u_x,
+    FP* dvc_u_y,
+    const FP omega,
+    const FP u_lid,
+    const uint32_t N_X, const uint32_t N_Y,
+    const uint32_t N_CELLS,
+    const bool write_rho,
+    const bool write_u_x,
+    const bool write_u_y)
+{
+    InitializeConstants();
+
+    const uint32_t N_GRIDSIZE = (N_CELLS + N_BLOCKSIZE - 1) / N_BLOCKSIZE;
+
+    ComputeFullyFusedOperations_K<N_DIR, N_BLOCKSIZE><<<N_GRIDSIZE, N_BLOCKSIZE>>>(
+        dvc_df, dvc_df_next, dvc_rho, dvc_u_x, dvc_u_y, omega, u_lid, N_X, N_Y,
+        N_CELLS, write_rho, write_u_x, write_u_y);
+
+    // wait for GPU to finish operations
+    cudaDeviceSynchronize();
+
+    if (!kernelAttributesDisplayed)
+    {
+        DisplayKernelAttributes(ComputeFullyFusedOperations_K<N_DIR, N_BLOCKSIZE>,
+            fmt::format("ComputeFullyFusedOperations_K<{}, {}>", N_DIR, N_BLOCKSIZE));
+
+        kernelAttributesDisplayed = true;
+    }
+
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess)
+    {
+        SPDLOG_ERROR("CUDA kernel failed: {}", cudaGetErrorString(err));
+    }
+}
