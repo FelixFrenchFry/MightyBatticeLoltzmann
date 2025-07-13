3dc8578cc09997ad8cdc5c35fa188ab67e901905
Felix <leimenstoll.felix@gmail.com>
2025-07-13 11:08:47 +0200

    reduced plot script memory consumption and improved efficiency

--- implementations/cuda_mpi/tools/visualizer_04.py
+++ implementations/cuda_mpi/tools/visualizer_04.py
@@ -10,23 +10,23 @@ FP = np.float64 if "--FP64" in sys.argv else np.float32
 # TODO: use quiver plot or stream plot?
 # ----- VISUALIZATION OF THE VELOCITY VECTOR STREAMLINES -----
 # simulation config
-N_X =   1000
-N_Y =   1000
-omega = 1.7
+N_X =   12000
+N_Y =   12000
+omega = 1.2
 u_lid = 0.1
 
 # step config
 step_start =    1
-step_end =      200_000
-step_stride =   50_000
+step_end =      17_000_000
+step_stride =   1_000_000
 steps = [1] + list(range(step_stride, step_end + 1, step_stride))
 
 # output path config
 dataType_A =        "velocity_x"
 dataType_B =        "velocity_y"
 outputDirName =     "output"
-versionDirName =    "06"
-subDirName =        "Y"
+versionDirName =    "04"
+subDirName =        "K"
 
 # formatting helper
 def format_step_suffix(step: int, width: int = 9) -> str:
@@ -38,6 +38,10 @@ def get_file_path(data: str, step: int) -> str:
         f"../../../exported/"
         f"{versionDirName}/{subDirName}/{data}{format_step_suffix(step)}.bin")
 
+# lazy loading
+def load_velocity_component(filename: str, dtype: np.dtype) -> np.ndarray:
+    return np.memmap(filename, dtype=dtype, mode='r', shape=(N_Y, N_X))
+
 # misc
 outputDir = f"{outputDirName}/{versionDirName}/{subDirName}"
 os.makedirs(outputDir, exist_ok=True)
@@ -46,8 +50,8 @@ stride_plot = 20
 for step in steps:
     try:
         # load velocity data
-        u_x = np.fromfile(get_file_path(dataType_A, step), dtype=FP).reshape((N_Y, N_X))
-        u_y = np.fromfile(get_file_path(dataType_B, step), dtype=FP).reshape((N_Y, N_X))
+        u_x = load_velocity_component(get_file_path(dataType_A, step), FP)
+        u_y = load_velocity_component(get_file_path(dataType_B, step), FP)
 
         # downsample data
         u_x_ds = u_x[::stride_plot, ::stride_plot]
@@ -79,6 +83,10 @@ for step in steps:
         outputPath = f"{outputDir}/streamlines{format_step_suffix(step)}.png"
         plt.savefig(outputPath, dpi=300)
         plt.close()
+        # free up memory
+        del u_x, u_y, u_x_ds, u_y_ds, X, Y, speed, strm, fig, ax
+        import gc; gc.collect()
+
         print(f"✅  Saved plot: {outputPath}")
 
     except Exception as e:
